78c9bb0f02f46081e20678fd8999cecee57fda3e
Kaiping <kaipingga@vuw.leidenuniv.nl>
2016-06-07 16:11:08 +0200

    write some basic code to fill the database with typological data

--- plld_app/scripts/initializedb.py
+++ plld_app/scripts/initializedb.py
@@ -8,12 +8,26 @@ from clld.db.models import common
 import plld_app
 from plld_app import models
 
+import os
+import os.path
+import pandas
+
+from parse_typology_questionnaire import parse_questionnaire, UnexpectedTypologyFormatError
+
+counter=0
+def newid():
+    global counter
+    counter += 1
+    return str(counter)
+
+DBPATH = "J:/ResearchData/HUM/LUCL-KlamerVICI/Lesser Sunda Database version 3 June 2016/"
 
 def main(args):
     data = Data()
 
     dataset = common.Dataset(
         id=plld_app.__name__,
+        name=plld_app.__name__,
         domain='plld.clld.org',
         description="Database of Papuan Language and Culture",
         )
@@ -22,22 +36,65 @@ def main(args):
     # All ValueSets must be related to a contribution:
     contrib = common.Contribution(id='contrib', name='the contribution')
 
-    # All ValueSets must be related to a Language:
-    lang = common.Language(
-        id='lang',
-        name='A Language',
-        latitude=20,
-        longitude=20)
+    # Load the list of languages
+    languages = pandas.ExcelFile(os.path.join(DBPATH, "Languages and Coordinates.xlsx")).parse(0)
+    
+    parameters = {}
+    for i, language in languages.iterrows():
+        # Generate the database object for each language
+        print("\nCreating language", language['Language name (-dialect)'])
+        lang = common.Language(
+            id=((language['Language name (-dialect)'].lower()[:4] + "x" + newid())
+                if pandas.isnull(language['Glottolog'])
+                else language['Glottolog'].strip()),
+            name=language['Language name (-dialect)'].strip(),
+            latitude=language['Lat'],
+            longitude=language['Lon'])
 
-    param = common.Parameter(id='param', name='Feature 1')
+        # Check what data files we have that say they are about that language.
+        try:
+            files_concerning = [file for file in os.listdir(DBPATH)
+                                if file.lower().startswith(language['ISO'].lower())]
+        except AttributeError:
+            continue
 
-    # ValueSets group Values related to the same Language, Contribution and 
-    # Parameter
-    vs = common.ValueSet(id='vs', language=lang, parameter=param, contribution=contrib)
+        typology_files = [file for file in files_concerning
+                          if 'typolog' in file.lower()]
 
-    # Values store the actual "measurements":
-    DBSession.add(common.Value(id='v1', name='value 1', valueset=vs))
-    DBSession.add(common.Value(id='v2', name='value 2', valueset=vs))
+        # Try to load the corresponding typology questionnaire
+        if len(typology_files) != 1:
+            continue
+        questionnaire = typology_files[0]
+        try:
+            contribution_text, parameter_descriptions, answers = parse_questionnaire(
+                os.path.join(DBPATH, questionnaire))
+            for p, parameter in parameter_descriptions.iterrows():
+                question = parameter['q_text']
+                answer = str(answers["a_text"][question])
+                #print('•', question, "=", answer.encode('ascii', 'ignore').decode('ascii'))
+                try:
+                    param = parameters[question.lower()]
+                except KeyError:
+                    param = common.Parameter(
+                        id='param{:s}'.format(newid()),
+                        name=question)
+                    parameters[question.lower()] = param
+
+                # ValueSets group Values related to the same Language, Contribution and 
+                # Parameter
+                vs = common.ValueSet(id='vs'+newid(),
+                                     language=lang,
+                                     parameter=param,
+                                     contribution=contrib)
+
+                # Values store the actual "measurements":
+                DBSession.add(common.Value(id='v'+newid(), name=answer, valueset=vs))
+                DBSession.flush()
+                
+            
+            print("Typology database for", language.name, "successfully loaded.")
+        except UnexpectedTypologyFormatError:
+            print("File", questionnaire, "had an unexpected format for a typology questionnaire!")
 
 def prime_cache(args):
     """If data needs to be denormalized for lookup, do that here.
---
+++ plld_app/scripts/parse_typology_questionnaire.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+""" Load typology questionnaires and parse them into a working format """
+
+import pandas
+from xlrd.biffh import XLRDError
+
+class UnexpectedTypologyFormatError (ValueError):
+    """Exception to be thrown when a questionnaire is misformatted"""
+    pass
+
+def parse_questionnaire(filename):
+    questionnaire = pandas.ExcelFile(filename)
+
+    metadata_sheet_name = 'Metadata'
+    if metadata_sheet_name in questionnaire.sheet_names:
+        metadata_sheet = questionnaire.parse(metadata_sheet_name, header=None)
+        try:
+            metadata_blob = "; ".join(metadata_sheet[0])
+        except KeyError:
+            metadata_blob = ""
+    else:
+        metadata_blob = ""
+    metadata = metadata_blob
+
+    weird_rows = set()
+    answers = pandas.DataFrame(columns = ["a_text", "a_notes" ,"a_reference"])
+    features = pandas.DataFrame(columns = ["q_id: Reesink", "q_text", "q_note: Nijmegen Typological Survey"])
+    data_sheet_name = 'Questionnaire'
+    try:
+        data_sheet = questionnaire.parse(
+            data_sheet_name,
+            skiprows=[0, 1, 2, 3, 4, 5, 7]).dropna('columns', 'all')
+    except XLRDError:
+        raise UnexpectedTypologyFormatError("Typology sheet did not have a 'Questionnaire' sheet")
+    for i, row in data_sheet.iterrows():
+        if pandas.isnull(row["q_text"]):
+            weird_rows.add(i)
+        if pandas.isnull(row["a_text"]):
+            weird_rows.add(i)
+        else:
+            features = features.append(row[["q_id: Reesink", "q_text", "q_note: Nijmegen Topological Survey"]])
+            answer = row[["a_text", "a_notes", "a_reference"]]
+            answer.name = row["q_text"]
+            answers = answers.append(answer)
+            assert(len(features) == len(answers))
+    return metadata, features, answers
+
+if __name__=='__main__':
+    import sys
+    metadata, features, answers = parse_questionnaire(sys.argv[1])
+    print(metadata)
+    print(features)
+    print(answers.to_string())
