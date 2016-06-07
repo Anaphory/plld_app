0e43cc74f3c92243b76fc1188a2d531f2a474ed9
Kaiping <kaipingga@vuw.leidenuniv.nl>
2016-06-07 10:04:56 +0200

    Use @xrotwang's minimal example to get a working data point

--- plld_app/scripts/initializedb.py
+++ plld_app/scripts/initializedb.py
@@ -12,9 +12,32 @@ from plld_app import models
 def main(args):
     data = Data()
 
-    dataset = common.Dataset(id=plld_app.__name__, domain='plld_app.clld.org')
+    dataset = common.Dataset(
+        id=plld_app.__name__,
+        domain='plld.clld.org',
+        description="Database of Papuan Language and Culture",
+        )
     DBSession.add(dataset)
 
+    # All ValueSets must be related to a contribution:
+    contrib = common.Contribution(id='contrib', name='the contribution')
+
+    # All ValueSets must be related to a Language:
+    lang = common.Language(
+        id='lang',
+        name='A Language',
+        latitude=20,
+        longitude=20)
+
+    param = common.Parameter(id='param', name='Feature 1')
+
+    # ValueSets group Values related to the same Language, Contribution and 
+    # Parameter
+    vs = common.ValueSet(id='vs', language=lang, parameter=param, contribution=contrib)
+
+    # Values store the actual "measurements":
+    DBSession.add(common.Value(id='v1', name='value 1', valueset=vs))
+    DBSession.add(common.Value(id='v2', name='value 2', valueset=vs))
 
 def prime_cache(args):
     """If data needs to be denormalized for lookup, do that here.
